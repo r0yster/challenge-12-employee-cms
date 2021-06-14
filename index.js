fe82adca05436e16b144de3a31be4056c759c0b5
royster <rcantu786@gmail.com>
2021-06-13 20:31:10 -0500

    finalized adding functions

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const inquirer = require('inquirer');
-const { exitCode } = require('process');
+const { exitCode, allowedNodeEnvironmentFlags } = require('process');
 const db = require('./db/connection');
 const cTable = require('console.table');
 
+// View Functions
 function viewDepartments() {
     const sql = `SELECT name AS Department_Name, id AS Department_Id 
                  FROM department`;
@@ -53,6 +54,92 @@ function viewEmployees() {
     appInit();
 };
 
+// // Add Functions
+function addDepartment() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'dpt_name',
+            message: 'Please enter new department name:'
+        }
+    ]).then( newDepartment => {
+        const sql = `INSERT INTO department (name) VALUES (?)`;
+        const params = newDepartment.dpt_name;
+
+        db.query(sql, params, (err, result) => {
+            if (err) throw err;
+            console.log('New Department Added');
+            appInit();
+        });
+    });
+};
+
+function addRole() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Please enter new role name:'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Please enter new role salary:'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter new role department (department id):'
+
+        }
+    ])
+    .then( newRole => {
+        const sql = `INSERT INTO emp_roles (title, salary, department_id) VALUES (?, ?, ?)`;
+        const params = [newRole.title, newRole.salary, newRole.id];
+
+        db.query(sql, params, (err, result) => {
+            if (err) throw err;
+            console.log('New role Added');
+            appInit();
+        });
+    });
+};
+
+function addEmployee() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'f_name',
+            message: 'Enter new employees First Name:'
+        },
+        {
+            type: 'input',
+            name: 'l_name',
+            message: 'Enter new employees Last Name:'
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'Enter new employees role (role id):'
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'Enter new employees manager (employee id):'
+        }
+    ])
+    .then(newEmployee => {
+        const sql  = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+        const params = [newEmployee.f_name, newEmployee.l_name, newEmployee.role, newEmployee.manager];
+
+        db.query(sql, params, (err, result) => {
+            if (err) throw err;
+            console.log('New Employee Added');
+            appInit();
+        });
+    });
+};
+
 function appInit() {
     return inquirer.prompt([
         {
@@ -76,6 +163,15 @@ function appInit() {
                 break;
             case "View all employees":
                 viewEmployees();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add an employee":
+                addEmployee();
                 break;
             case "Exit":
                 process.exit(1);
